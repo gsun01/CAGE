55977c2ec83f9acc380b1165954d626e70033ee1
Guotong Sun <guotongsun01@gmail.com>
2025-08-21 20:33:37 -0500

    initial commit

---
+++ j_eff.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+import numpy as np
+
+# __all__ = [
+#     "initialize_bessel_table",
+#     "BesselJZeros", "BesselJpZeros",
+#     "get_bessel_tables", "is_initialized",
+# ]
+
